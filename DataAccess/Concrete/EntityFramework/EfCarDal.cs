b6a25692cbd7e460b3d9aa8c7c20306f8ddb9e1e
s3m1h <semih_acar01@hotmail.com>
2022-02-19 13:06:34 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                // eklenecek varlık
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                // silinecek varlık
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                // filtre verilmemişse tüm carları listeye çevirip getir
+                // filtre verilmişse filtreleyip getir
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public Car GetById(int id)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                return context.Set<Car>().SingleOrDefault(c => c.Id == id);
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                // update edilecek varlık
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1,BrandId = 1, ColorId = 1,ModelYear=2000},
+                new Car{ Id = 2,BrandId = 1, ColorId = 3,ModelYear=2002},
+                new Car{ Id = 3,BrandId = 2, ColorId = 3,ModelYear=2003},
+                new Car{ Id = 4,BrandId = 2, ColorId = 5,ModelYear=2004},
+                new Car{ Id = 5,BrandId = 3, ColorId = 5,ModelYear=2005},
+                new Car{ Id = 6,BrandId = 3, ColorId = 7,ModelYear=2006},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car car1 = _cars.SingleOrDefault(c=>c.Id ==car.Id);
+            _cars.Remove(car1);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car car1 = _cars.SingleOrDefault(c => c.Id == car.Id);
+            car1.ModelYear = car.ModelYear;
+            car1.BrandId = car.BrandId;
+            car1.ColorId = car.ColorId;
+            car1.DailyPrice = car.DailyPrice;
+            car1.Description = car.Description;
+        }
+    }
+}
